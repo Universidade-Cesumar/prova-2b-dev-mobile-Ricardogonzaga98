c78e69d75d6a71d768babd85b15977bb3082aed5
Joao <ricardogonzaga98@gmail.com>
2026-06-27 16:05:00 -0300

    feat: criar App.js como ponto de entrada renderizando HomeScreen

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { HomeScreen } from './src/screens/HomeScreen';
+
+/**
+ * Ponto de entrada do aplicativo.
+ * Renderiza a tela principal do Almoxarifado.
+ */
+export default function App() {
+  return <HomeScreen />;
+}
