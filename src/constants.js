7461b98946d96ed1200644eb7b9b478b2e0c7e57
Joao <ricardogonzaga98@gmail.com>
2026-06-27 16:34:55 -0300

    feat: adicionar constante APP_VERSION para controle de versao do app

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL = 'https://6a2b4338b687a7d5cbc50ae9.mockapi.io';
 export const ENDPOINT_MATERIAIS = '/materiais';
 export const LIMITE_ESTOQUE_CRITICO = 10;
+export const APP_VERSION = '1.0.0';
