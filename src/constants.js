6aac5aac16add73ac83f9663a2a7aa07a9ce48c6
Joao <ricardogonzaga98@gmail.com>
2026-06-27 16:30:42 -0300

    fix: ajustar limite de estoque critico para 10 unidades conforme regra de negocio

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = 'https://6a2b4338b687a7d5cbc50ae9.mockapi.io';
 export const ENDPOINT_MATERIAIS = '/materiais';
-export const LIMITE_ESTOQUE_CRITICO = 5;
+export const LIMITE_ESTOQUE_CRITICO = 10;
