75e98c0a83288eb6445a717ab4be8f3399ceca2c
Joao <ricardogonzaga98@gmail.com>
2026-06-27 16:30:21 -0300

    feat: definir limite de estoque critico como 5 unidades

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,3 @@
-/**
- * Constantes globais do aplicativo
- */
-
-// URL base da MockAPI usada pelo projeto
 export const API_BASE_URL = 'https://6a2b4338b687a7d5cbc50ae9.mockapi.io';
-
-// Endpoint de materiais
 export const ENDPOINT_MATERIAIS = '/materiais';
-
-// Limite de quantidade considerado "estoque crítico"
-export const LIMITE_ESTOQUE_CRITICO = 10;
+export const LIMITE_ESTOQUE_CRITICO = 5;
